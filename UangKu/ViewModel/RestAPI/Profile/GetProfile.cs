430ef82595f6cdeeb2adb523fe7de79f4fe3722c
IlhamHrnd <ilhamhernadi88@gmail.com>
2023-10-21 23:48:54 +0700

    Add new endpoint
    - Penambahan class untuk memanggil endpoint GetProfile dari webservice

---
+++ UangKu/ViewModel/RestAPI/Profile/GetProfile.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using UangKu.Model.Base;
+using static UangKu.Model.Response.Profile.Profile;
+
+namespace UangKu.ViewModel.RestAPI.Profile
+{
+    public class GetProfile
+    {
+        private const string ProfileEndPoint = "https://uangkuapi.azurewebsites.net/Profile/GetPersonID?PersonID={0}";
+
+        public static async Task<ProfileRoot> GetProfileID(string personID)
+        {
+            ProfileRoot root = new ProfileRoot();
+            string url = string.Format(ProfileEndPoint, personID);
+            var client = new RestClient(url);
+            var request = new RestRequest
+            {
+                Method = Method.Get,
+                Timeout = ParameterModel.ItemDefaultValue.Timeout
+            };
+            var response = await client.ExecuteGetAsync(request);
+
+            try
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = response.Content;
+                    var format = content.Substring(1, content.Length - 2);
+                    var get = JsonConvert.DeserializeObject<ProfileRoot>(format);
+                    root = get;
+                }
+                else
+                {
+                    await MsgModel.MsgNotification(response.ErrorMessage);
+                }
+            }
+            catch (Exception e)
+            {
+                await MsgModel.MsgNotification(e.Message);
+            }
+            return root;
+        }
+    }
+}
